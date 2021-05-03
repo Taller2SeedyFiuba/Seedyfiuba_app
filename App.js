e72ee2eb5ffb834e84acdc6f0ffd769d08c25469
FrancoBar <44034749+FrancoBar@users.noreply.github.com>
2021-05-03 20:58:26 -0300

    Agrego ayuda y tema

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
+import {DefaultTheme,  Provider as PaperProvider } from 'react-native-paper';
 import { createStackNavigator } from '@react-navigation/stack';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
+};
 
 import {Main} from './src/Main.js';
 
 export default function App() { 
   return (
-  	<PaperProvider>
+  	<PaperProvider theme={theme}>
 	    <Main/>
   	</PaperProvider>
   ); 
--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,23 +1,34 @@
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+import { HelperText, TextInput, Button } from 'react-native-paper';
 
 function SignIn({ navigation }) {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
+  
+  const hasInvalidUser = () => {
+    return !username.includes('@');
+  };
+
+  const hasInvalidPassword = () => {
+    return (password.length < 5);
+  };
 
   return (
     <View style={style.container}>
       <TextInput
-        label='usuario'
+        label='Usuario'
         mode='outlined'
         dense={true}
         style={{margin:15}}
         value={username}
         onChangeText={username => setUsername(username)}
       />
+      <HelperText type="error" visible={hasInvalidUser()}>
+        Email inválido.
+      </HelperText>
       <TextInput
-        label='contraseña'
+        label='Contraseña'
         mode='outlined'
         dense={true}
         style={{margin:15}}
@@ -25,6 +36,9 @@ function SignIn({ navigation }) {
         secureTextEntry={true}
         onChangeText={password => setPassword(password)}
       />
+      <HelperText type="error" visible={hasInvalidPassword()}>
+        Contraseña muy corta.
+      </HelperText>
       
       <View style={StyleSheet.container, {alignItems: 'center'}}>
         <Button
