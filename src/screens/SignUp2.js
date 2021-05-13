47d565f240176f4d4b0ca950a090abf5765cf045
guidobotta <gbotta@fi.uba.ar>
2021-05-13 00:09:25 -0300

    Agrego calendario

--- src/screens/SignUp2.js
+++ src/screens/SignUp2.js
@@ -1,16 +1,30 @@
 import * as React from 'react';
 import { View, StyleSheet} from 'react-native';
-import { HelperText, Button } from 'react-native-paper';
+import { HelperText, Button, TextInput } from 'react-native-paper';
 import * as Auth from './../providers/auth-provider.js';
 import * as Client from  './../providers/client-provider.js';
 import { SignInput } from './../SignComp.js';
 import {showInvalidName, showInvalidBirthDate, showRegisterError } from './../SignErrors.js';
+import { DatePickerModal } from 'react-native-paper-dates';
 
 function SignUp2 ({ navigation, email}) {
   const [errorInfo, setErrorInfo] = React.useState('');
   const [firstName, setFirstName] = React.useState('');
   const [lastName, setLastName] = React.useState('');
-  const [birthDate, setBirthDate] = React.useState('');
+  const [birthDate, setBirthDate] = React.useState(undefined);
+  const [open, setOpen] = React.useState(false);
+
+  const onDismissSingle = React.useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+  const onConfirmSingle = React.useCallback(
+    (params) => {
+      setOpen(false);
+      setBirthDate(params.date);
+    },
+    [setOpen, setBirthDate]
+  );
 
   const signUp2Register = () => {
     if(!showInvalidName(firstName) && !showInvalidName(firstName)){
@@ -45,16 +59,28 @@ function SignUp2 ({ navigation, email}) {
         secureTextEntry={false}
       />
 
-      <SignInput
-        aLabel='Fecha de nacimiento'
-        onChangeValue={birthDate => setBirthDate(birthDate)}
-        aValue={birthDate}
-        textError='Fecha de nacimiento inválida.'
-        showInvalidValue={showInvalidBirthDate(birthDate)}
-        secureTextEntry={false}
+      <TextInput
+      label={birthDate ? `${birthDate.getDate()}/${birthDate.getMonth()+1}/${birthDate.getFullYear()}` : 'Fecha de nacimiento'}
+      mode='outlined'
+      dense={true}
+      style={{margin:15}}
+      disabled={false}
+      onFocus={() => setOpen(true)}
       />
+      <DatePickerModal
+          mode="single"
+          visible={open}
+          onDismiss={onDismissSingle}
+          birthDate={birthDate}
+          onConfirm={onConfirmSingle}
+          saveLabel="Guardar" // optional
+          label="Fecha Seleccionada" // optional
+          validRange={{
+            endDate: new Date(2021, 1, 1), // optional
+          }}
+        />
 
-      <View style={StyleSheet.container, {alignItems: 'center'}}>
+      <View style={{alignItems: 'center'}}>
         <Button
             mode="contained"
             color="green"
