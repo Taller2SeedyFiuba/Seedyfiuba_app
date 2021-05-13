c30fcd7070db6062f7fd9cdcd8f27e56e188c7ee
FrancoBar <44034749+FrancoBar@users.noreply.github.com>
2021-05-12 23:08:01 -0300

    Agrego animación y quito títulos

--- src/Main.js
+++ src/Main.js
@@ -17,11 +17,11 @@ const Main = () =>{
 
     return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: true, animationEnabled:true, title : ''}}>
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="SignUp" component={SignUp}/>
         <Stack.Screen name="SignUp2" component={SignUp2} />
-        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignIn" component={SignIn}/>
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
