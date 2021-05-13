51b1be9b2a48d51ae0c7fca139a2ec7d2e3ac319
guidobotta <gbotta@fi.uba.ar>
2021-05-13 01:07:40 -0300

    Actualizo screen Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,40 @@
 import * as React from 'react';
-import { View, StyleSheet, Image, Text } from 'react-native';
-import { Button, List } from 'react-native-paper';
+import { View } from 'react-native';
+import { Text, BottomNavigation } from 'react-native-paper';
 
+const HomeRoute = () => {
+	return(
+		<View>
+			<Text>Home</Text>
+		</View>
+	)
+}
+
+const AccountRoute = () => {
+	return(
+		<View>
+			<Text>Account</Text>
+		</View>
+	)
+}
 
 export function Home({ navigation }) {
-	return (
-		<View 
-		style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      	}}>
-			<Text> Bienvenido </Text>
-		</View>
-	);
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'home', title: 'Home', icon: 'home' },
+    { key: 'account', title: 'Account', icon: 'account' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    home: HomeRoute,
+    account: AccountRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
 }
