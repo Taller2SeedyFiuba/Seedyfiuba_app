9f0ee1110f417381351033b4b09a83ffdddf97af
guidobotta <gbotta@fi.uba.ar>
2021-05-13 17:00:07 -0300

    Agrego screens de Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,6 +10,22 @@ function HomeRoute () {
 	)
 }
 
+function SearchRoute () {
+	return(
+		<View style={styles.container}>
+			<Text>Search</Text>
+		</View>
+	)
+}
+
+function MessageRoute () {
+	return(
+		<View style={styles.container}>
+			<Text>Message</Text>
+		</View>
+	)
+}
+
 function AccountRoute () {
 	return(
 		<View style={styles.container}>
@@ -29,18 +45,22 @@ const styles = StyleSheet.create({
 function Home({ navigation }) {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'home', title: 'Home', icon: 'home' },
-    { key: 'account', title: 'Account', icon: 'account' },
+    { key: 'home', title: 'Inicio', icon: 'home' },
+    { key: 'search', title: 'Búsqueda', icon: 'magnify' },
+    { key: 'message', title: 'Mensajes', icon: 'chat' },
+    { key: 'account', title: 'Cuenta', icon: 'account' },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
     home: HomeRoute,
+    search: SearchRoute,
+    message: MessageRoute,
     account: AccountRoute,
   });
 
   return (
     <BottomNavigation
-      barStyle={{ backgroundColor: '#008F39' }}
+      barStyle={{ backgroundColor: '#77A656' }}
       navigationState={{ index, routes }}
       onIndexChange={setIndex}
       renderScene={renderScene}
