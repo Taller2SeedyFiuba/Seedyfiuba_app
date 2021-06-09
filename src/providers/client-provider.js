be30716b8f1d7ea091b2860f6c6868cad7219ae1
FrancoBar <44034749+FrancoBar@users.noreply.github.com>
2021-06-09 16:00:28 -0300

    Nuevos urls

--- src/providers/client-provider.js
+++ src/providers/client-provider.js
@@ -1,4 +1,4 @@
-import {USERS_URL, USERS_ME_URL} from '@env';
+import {USERS_URL, USERS_ME_URL, PROJECT_NEW_URL, PROJECT_ME_URL, PROJECT_ID_URL} from '@env';
 
 function sendData(url, token, data){
   return fetch(url, {
@@ -33,10 +33,28 @@ function getData(url, token){
   });
 };
 
+
+//Users
+
 export async function getUserData(token){
   return await getData(USERS_ME_URL, token).catch((error) => {throw error});
 }
 
 export async function sendUserData(token, data){
   return await sendData(USERS_ME, token, data).catch((error) => {throw error});
+}
+
+
+//Projects
+
+export async function getProjectsMe(token){
+  return await getData(PROJECT_ME_URL, token).catch((error) => {throw error});
+}
+
+export async function getProjectsID(token, id){
+  return await getData(PROJECT_ID_URL + id, token).catch((error) => {throw error});
+}
+
+export async function sendNewProject(token, data){
+  return await sendData(PROJECT_NEW_URL, token, data).catch((error) => {throw error});
 }
