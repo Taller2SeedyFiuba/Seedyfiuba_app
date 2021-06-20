34dfa5ae356623b38f2d02f50b3461352d1b5ee5
FrancoBar <44034749+FrancoBar@users.noreply.github.com>
2021-06-20 17:55:30 -0300

    Cambios en búsqueda

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,26 +1,31 @@
 import * as React from 'react';
 import { View } from 'react-native';
-import { List, Searchbar, RadioButton, Appbar } from 'react-native-paper';
+import { Button,Text, TextInput, List, Searchbar, RadioButton, Appbar } from 'react-native-paper';
 import * as Client from  './../providers/client-provider.js';
 import * as Auth from '../providers/auth-provider.js';
 import { ProjectListComponent } from './../components/ProjectListComponent.js';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 function Search ({navigation}) {
+  const [visibleMenu, setVisibleMenu] = React.useState(false);
   const [searchQuery, setSearchQuery] = React.useState('');
   const [option, setOption] = React.useState('Proyect Geographic')
   const [data, setData] = React.useState([]);
+  const [location, setLocation] = React.useState('');
 
   const viewProjectCallback = (id) => {
     navigation.navigate('ProjectInfo', {projectId : id});
   };
 
-  const message = {
-    type : 'art'
-  };
+  const query = {
+      tags : ['Frio'],
+      limit : 1,
+      page : 0
+    };
 
   React.useEffect(() => {
     Auth.getIdToken(true).then((token) => {
-    Client.getSearchProject(token, message).then((resp) =>{
+    Client.getSearchProject(token, query).then((resp) =>{
       var copy = [];
       resp.forEach((element) =>{
         var newElement = {};
@@ -41,15 +46,35 @@ function Search ({navigation}) {
     <Appbar.Header style={{height:50}}>
       <Appbar.Content title='Búsqueda'/>
     </Appbar.Header>
-      <View style={{justifyContent:'flex-start', flex:1, marginLeft: '10%', maxWidth: '80%'}}>
-      <View style={{justifyContent:'center', flex:1}}>
+      <View style={{justifyContent:'flex-start', marginLeft: '10%', maxWidth: '80%'}}>
+      <View style={{justifyContent:'center', flexDirection: 'row'}}>
         <Searchbar
           placeholder='Buscar'
           onChangeText={searchQuery => setSearchQuery(searchQuery)}
           value={searchQuery}
         />
+        <Button mode='contained' onPress={() => setVisibleMenu(!visibleMenu)}> : </Button>
       </View>
-      <View style={{justifyContent:'flex-start', flex:3}}>
+      {visibleMenu && <View style={{justifyContent:'flex-start', flex:3}}>
+                <GooglePlacesAutocomplete // ARREGLAR, NO FUNCIONA
+                    onPress={(data, details = null) => {
+                        setLocation(data.description);
+                    }}
+                    query={{
+                        key: 'AIzaSyDlPVGnR9jYlGObED64_d5HMO88YN0yz5A',
+                        language: 'en',
+                    }}
+                    textInputProps={{
+                        InputComp: TextInput,
+                        label:'Ubicacion',
+                        mode:'outlined',
+                        dense:true,
+                        style:{marginVertical:15, flex:1},
+                        value: location,
+                        onChangeText: location => setLocation(location),
+                        left:<TextInput.Icon name='earth'/>,
+                      }}
+                />
         <List.Section title='Tipo de Búsqueda'>
           <RadioButton.Group
           value={option}
@@ -61,7 +86,7 @@ function Search ({navigation}) {
           <RadioButton.Item label='Usuario' value='User' mode='android' />
           </RadioButton.Group>
         </List.Section>
-      </View>
+      </View>}
     </View>
     <ProjectListComponent data = {data}
                                   viewProjectCallback = {viewProjectCallback}
