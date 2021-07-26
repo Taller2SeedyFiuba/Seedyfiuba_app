869b1c11306f5b9c41166e32d7c01c79af957397
FrancoBar <44034749+FrancoBar@users.noreply.github.com>
2021-07-26 18:32:28 -0300

    Creo Confirmation Boxes

---
+++ src/components/ConfirmationBoxes.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Dialog, Text, Button, Portal } from 'react-native-paper';
+
+export function ConfirmationBox(props) {
+    return (
+    <Portal>
+        <Dialog visible={props.visible} onDismiss={() => props.setVisible(false)}>
+          <Dialog.Title>props.title</Dialog.Title>
+          <Dialog.Content>
+            <Text>prop.description</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={props.onCancel}>Cancelar</Button>
+            <Button onPress={props.onContinue}>Continuar</Button>
+          </Dialog.Actions>
+        </Dialog>
+    </Portal>
+    )
+}
+
+export function AcceptBox(props) {
+    return (
+    <Portal>
+        <Dialog visible={props.visible} onDismiss={() => props.setVisible(false)}>
+          <Dialog.Title>props.title</Dialog.Title>
+          <Dialog.Content>
+            <Text>prop.description</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={props.onAccept}>Aceptar</Button>
+          </Dialog.Actions>
+        </Dialog>
+    </Portal>
+    )
+}
